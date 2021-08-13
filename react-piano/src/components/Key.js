5c6f2502a5d275299762274d9398c24e4ee78506
Geraldine <hello@geraldiner.com>
2021-08-12 20:52:54 -1000

    NOW WE HAVE FEEDBACK

--- react-piano/src/components/Key.js
+++ react-piano/src/components/Key.js
@@ -1,8 +1,9 @@
 import React from "react";
 import isAccidental from "../utils/isAccidental";
 
-const Key = ({ note, keyboardKey }) => {
-	return <div className={isAccidental(note) ? `key black` : `key white`}>{keyboardKey}</div>;
+const Key = ({ note, keyboardKey, isPlaying }) => {
+	const keyClass = isAccidental(note) ? "key black" : "key white";
+	return <div className={isPlaying ? `${keyClass} active` : keyClass}>{keyboardKey}</div>;
 };
 
 export default Key;
--- react-piano/src/components/Piano.js
+++ react-piano/src/components/Piano.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AudioPlayer from "../core/AudioPlayer";
 import Key from "./Key";
 import getNotesBetween from "../utils/getNotesBetween";
@@ -7,30 +7,36 @@ import MAPS from "../constants/KEYBOARD_MAP";
 const Piano = ({ startNote, endNote }) => {
 	const notes = getNotesBetween(startNote, endNote);
 	const audioPlayer = AudioPlayer();
+
+	const [notesPlaying, setNotesPlaying] = useState([]);
 	useEffect(() => {
 		audioPlayer.setInstrument("acoustic_grand_piano");
 		window.addEventListener("keydown", handleKeyDown);
 		// window.addEventListener("keyup", handleKeyUp);
+		console.log(notesPlaying);
 	}, []);
 
 	const handleKeyDown = e => {
 		if (!e.repeat) {
 			const note = MAPS.KEY_TO_NOTE[e.key.toUpperCase()];
 			if (note) {
+				setNotesPlaying([...notesPlaying, note]);
 				audioPlayer.playNote(note);
 			}
 		}
 	};
-	// const handleKeyUp = e => {
-	// 	if (!e.repeat) {
-	// 		if (e.key === "a") {
-	// 		}
-	// 	}
-	// };
+	const handleKeyUp = e => {
+		if (!e.repeat) {
+			const note = MAPS.KEY_TO_NOTE[e.key.toUpperCase()];
+			if (note) {
+				setNotesPlaying([notesPlaying.filter(notePlaying => notePlaying !== note)]);
+			}
+		}
+	};
 	return (
 		<div className="piano">
 			{notes.map((note, index) => {
-				return <Key key={index} note={note} keyboardKey={MAPS.NOTE_TO_KEY[note]} />;
+				return <Key key={index} note={note} keyboardKey={MAPS.NOTE_TO_KEY[note]} isPlaying={notesPlaying.includes(note)} />;
 			})}
 		</div>
 	);
